f11dcf590888f9c2d321acfbf8d7a7a66930c065
mdazlaanzubair <mdazlaan1996@gmail.com>
2024-04-10 02:09:01 +0500

    navigation fixed

--- app/components/navigation/SideBar.jsx
+++ app/components/navigation/SideBar.jsx
@@ -1,11 +1,13 @@
 "use client";
 
-import React from "react";
+import React, { useContext } from "react";
 import SpotlightLink from "./SpotlightLink";
 import Link from "next/link";
 import { navList } from "./navigation-list";
 import SocialNavigation from "./SocialNavigation";
 import { motion } from "framer-motion";
+import { GeneralContext } from "@/app/context/GeneralContext";
+import { usePathname, useRouter } from "next/navigation";
 
 const contentContainerVariants = {
   hidden: { opacity: 0 },
@@ -30,23 +32,117 @@ const contentVariants = {
 };
 
 const SideBar = () => {
+  const router = useRouter();
+  const activeSection = usePathname();
+  const { toggleMenu, isMenuOpen } = useContext(GeneralContext);
+
   return (
-    <div className="hidden relative w-2/12 lg:flex flex-col items-start h-full max-h-full p-3 rounded-xl">
-      <Link href="/" className="nav-link group gap-4 hover:bg-primary/10">
-        <img
-          className="w-10 rounded-full"
-          src="https://framerusercontent.com/images/t4kKb01sNaira2a5mAbB9b1PBIw.jpg"
-        />
-        <div className="profile">
-          <h1 className="font-semibold text-secondary text-sm leading-relaxed">
-            Azlaan
-          </h1>
-          <p className="font-medium text-primary group-hover:text-accent text-xs">
-            Web Engineer
-          </p>
-        </div>
-      </Link>
-      <div className="flex flex-col justify-between w-full h-full pt-10">
+    <div
+      className={`fixed top-14 bottom-14 left-0 flex flex-col items-center justify-between gap-3 z-50`}
+    >
+      <div
+        className={`flex translate-x-2 flex-col flex-shrink p-3 rounded-lg ${
+          isMenuOpen && activeSection !== "/"
+            ? "translate-x-2 opacity-100"
+            : "-translate-x-full -left-full opacity-0"
+        } transition-all ease-in-out duration-300`}
+      >
+        <button
+          onClick={() => router.back()}
+          className="relative  p-3 bg-base-100 text-primary rounded-lg"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className={`flex flex-col flex-grow p-3 items-center justify-center my-5 gap-3 rounded-lg ${
+          isMenuOpen
+            ? "translate-x-2 opacity-100"
+            : "-translate-x-full -left-full opacity-0"
+        } bg-base-300/80 border border-primary/20 transition-all ease-in-out duration-300`}
+      >
+        {navList?.map((nav, index) => (
+          <Link
+            href={nav?.url}
+            className="relative group flex items-center p-3 bg-base-100 text-primary hover:text-secondary rounded-xl transition-all ease-in-out duration-300"
+            title={nav?.title}
+          >
+            <div className="w-5 h-5">{nav?.icon()}</div>
+          </Link>
+        ))}
+        {/* <Link
+          href="/"
+          className="relative group flex items-center p-3 bg-base-100 rounded-xl"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z"
+            />
+          </svg>
+        </Link> */}
+      </div>
+      <div className="flex translate-x-2 flex-col flex-shrink p-3 rounded-lg">
+        <button
+          onClick={toggleMenu}
+          className="relative w-16 h-16 p-3 bg-base-100 text-primary rounded-lg"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`absolute inline-block w-8 h-8 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${
+              isMenuOpen ? "opacity-0 rotate-180" : "opacity-100 rotate-0"
+            } transition-all ease-in-out duration-500`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3.75 9h16.5m-16.5 6.75h16.5"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`absolute inline-block w-8 h-8 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 ${
+              isMenuOpen ? "opacity-100 rotate-0" : "opacity-0 -rotate-180"
+            } transition-all ease-in-out duration-500`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+      {/* <div className="flex flex-col justify-between w-full h-full pt-10">
         <motion.div
           variants={contentContainerVariants}
           initial="hidden"
@@ -70,7 +166,7 @@ const SideBar = () => {
           ))}
         </motion.div>
         <SocialNavigation />
-      </div>
+      </div> */}
     </div>
   );
 };
--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import Header from "./components/generic/Header";
 import Footer from "./components/generic/Footer";
 import GeneralProvider from "./context/GeneralContext";
 import { Red_Hat_Display, Inter } from "next/font/google";
+import MobileNavToggler from "./components/navigation/MobileNavToggler";
 
 const display = Red_Hat_Display({
   subsets: ["latin"],
@@ -23,13 +24,14 @@ export default function RootLayout({ children }) {
     <html lang="en" className="text-primary bg-base-300">
       <body className={`${display.variable} ${inter.variable}`}>
         <GeneralProvider>
-          <main className="font-inter flex flex-col lg:flex-row items-center bg-base-300 w-full h-[100vh] gap-5 p-3 lg:p-10 overflow-hidden">
+          <main className="font-inter relative flex flex-col items-center bg-base-300 w-full h-[100vh] p-5 overflow-hidden">
             <SideBar />
-            <Header />
-            <div className="view-port-container w-full h-full lg:w-10/12 bg-base-100/35 scroll-smooth">
+            {/* <Header /> */}
+            <div className="view-port-container w-full h-full bg-base-100/35 scroll-smooth">
               <div className="relative w-full h-auto">{children}</div>
               <Footer />
             </div>
+            {/* <MobileNavToggler /> */}
           </main>
         </GeneralProvider>
       </body>
