734efbfcf27c5b99213af7ef0d8c5f559cad5603
Muhammad Azlaan Zubair <mdazlaan1996@gmail.com>
2025-03-21 13:05:23 +0500

    Revert "Merge remote-tracking branch 'v3-origin/master'"
    This reverts commit 6c5a7524c0c9e96afd7fba770450da2f1afee768, reversing
    changes made to bf54645db7349563d42dde896d69aba9e6bdabc6.

--- app/utils/expCalculator.js
+++
@@ -1,6 +0,0 @@
-export default function calculateYearCount() {
-  const targetDate = new Date("2023-04-12");
-  const yearDifference =
-    (new Date() - targetDate) / (1000 * 60 * 60 * 24 * 365.25); // Dividing by the average number of milliseconds in a year
-  return yearDifference.toFixed(1);
-}
