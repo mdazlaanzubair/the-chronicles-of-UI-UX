8c0347d70a8b1d8c19e8834b9749855cbcd667a4
Muhammad Azlaan Zubair <mdazlaan1996@gmail.com>
2025-05-22 15:18:17 +0500

    minor fixings

--- src/pages/CredentialsPage.tsx
+++ src/pages/CredentialsPage.tsx
@@ -1,14 +1,3 @@
-import cobone_logo from "../assets//credentials-logo/cobone_logo.jpeg";
-import yallacompare_logo from "../assets//credentials-logo/yallacompare_logo.jpeg";
-import transviti_logo from "../assets//credentials-logo/transviti_logo.jpeg";
-import suparco_logo from "../assets//credentials-logo/suparco-logo.jpeg";
-import fast_logo from "../assets//credentials-logo/fast-logo.jpeg";
-import ilma_logo from "../assets//credentials-logo/ilma-logo.jpeg";
-import um_logo from "../assets//credentials-logo/um-logo.jpeg";
-import umsi_logo from "../assets//credentials-logo/umsi-logo.jpeg";
-import hkust_logo from "../assets//credentials-logo/hkust_logo.jpeg";
-import deeplearningai_logo from "../assets//credentials-logo/deeplearningai_logo.jpeg";
-
 import EducationCard from "../components/EducationCard";
 import ExperienceCard from "../components/ExperienceCard";
 import CertificateCard from "../components/CertificateCard";
@@ -16,152 +5,13 @@ import NoCredentials from "../components/NoCredentials";
 import FooterBtn from "../components/FooterBtn";
 import { MdArrowOutward } from "react-icons/md";
 
+import {
+  experiences,
+  education,
+  certifications,
+} from "../utils/constant_export";
+
 const CredentialsPage = () => {
-  const experiences = [
-    {
-      date: "September 2024 - Present",
-      title: "Frontend Developer",
-      company: {
-        name: "Cobone",
-        url: "https://www.cobone.com/en/",
-        logo: cobone_logo,
-      },
-      location: "Dubai, United Arab Emirates",
-      description: `Developed and maintained responsive UI components for Cobone.com’s e-commerce platform. Reduced page load times by 25% and boosted mobile engagement by 40%. Implemented real-time filtering and streamlined navigation to boost conversions.`,
-    },
-    {
-      date: "June 2024 - August 2024",
-      title: "Frontend Developer",
-      company: {
-        name: "YallaCompare",
-        url: "https://yallacompare.com/uae/en/",
-        logo: yallacompare_logo,
-      },
-      location: "Dubai, United Arab Emirates",
-      description: `Developed micro-frontend apps for Health and Travel services, improving performance by 20%, satisfaction by 35%, and reducing onboarding time by 25%.`,
-    },
-    {
-      date: "October 2023 - June 2024",
-      title: "Frontend Developer",
-      company: {
-        name: "Transviti",
-        url: "https://transviti.com/",
-        logo: transviti_logo,
-      },
-      location: "Riyadh, Saudi Arabia",
-      description: `Streamlined talent acquisition with TalentVare portals. Integrated Microsoft Entra ID with TaskVare, reducing errors by 30%. Automated LOADe delivery bookings, improving logistics efficiency by 15%.`,
-    },
-    {
-      date: "February 2020 - October 2023",
-      title: "IT Assistant",
-      company: {
-        name: "SUPARCO",
-        url: "https://www.linkedin.com/company/pakistan-space-and-upper-atmosphere-research-comission-suparco/about/",
-        logo: suparco_logo,
-      },
-      location: "Karachi, Pakistan",
-      description: `Reduced document processing time by 40% with a web-based approval system. Improved system uptime by 20%. Gained React.js experience, enhancing user satisfaction by 15%.`,
-    },
-  ];
-
-  const education = [
-    {
-      date: "August 2024 - September 2026",
-      degree: "Master's degree",
-      field: "Computer Software Engineering",
-      institute: {
-        name: "FAST University",
-        url: "https://khi.nu.edu.pk/",
-        logo: fast_logo,
-      },
-    },
-    {
-      date: "September 2016 - October 2020",
-      degree: "Bachelor of Science - BS",
-      field: "Computer Software Engineering",
-      institute: {
-        name: "ILMA University",
-        url: "https://ilmauniversity.edu.pk/",
-        logo: ilma_logo,
-      },
-    },
-  ];
-
-  const certifications = [
-    {
-      title: "Front-End Web UI Frameworks and Tools",
-      institute: {
-        name: "The Hong Kong University of Science and Technology",
-        url: "https://www.hkust.edu.hk/",
-        logo: hkust_logo,
-      },
-      date: "Jan 2025",
-      credentialId: "DT9KFAGVJ8EG",
-      certificate_url:
-        "https://www.coursera.org/account/accomplishments/certificate/DT9KFAGVJ8EG",
-    },
-    {
-      title: "Image Processing and OCR",
-      institute: {
-        name: "University of Michigan - School of Information",
-        url: "https://si.umich.edu/",
-        logo: umsi_logo,
-      },
-      date: "Aug 2019",
-      credentialId: "F42TMF48MXZP",
-      certificate_url:
-        "https://www.coursera.org/account/accomplishments/certificate/F42TMF48MXZP",
-    },
-    {
-      title: "Introduction to AI",
-      institute: {
-        name: "DeepLearning.AI",
-        url: "https://www.deeplearning.ai/",
-        logo: deeplearningai_logo,
-      },
-      date: "Apr 2019",
-      credentialId: "W2CWXZ2FHD3Q",
-      certificate_url:
-        "https://www.coursera.org/account/accomplishments/certificate/W2CWXZ2FHD3Q",
-    },
-    {
-      title: "date Structures in Python",
-      institute: {
-        name: "University of Michigan",
-        url: "https://umich.edu/",
-        logo: um_logo,
-      },
-      date: "Apr 2016",
-      credentialId: "33JDM48G7YVB",
-      certificate_url:
-        "https://www.coursera.org/account/accomplishments/certificate/33JDM48G7YVB",
-    },
-    {
-      title: "Web Structuring",
-      institute: {
-        name: "University of Michigan - School of Information",
-        url: "https://si.umich.edu/",
-        logo: umsi_logo,
-      },
-      date: "Apr 2016",
-      credentialId: "6UK5ZXUCXU72",
-      certificate_url:
-        "https://www.coursera.org/account/accomplishments/certificate/6UK5ZXUCXU72",
-    },
-    {
-      title: "Python Programming",
-      institute: {
-        name: "University of Michigan",
-        url: "https://umich.edu/",
-        logo: um_logo,
-      },
-      date: "Mar 2016",
-      credentialId: "893KLF5Y6NL9",
-      certificate_url:
-        "https://www.coursera.org/account/accomplishments/certificate/893KLF5Y6NL9",
-    },
-  ];
-
   return (
     <section
       id="credentials-page"
@@ -196,7 +46,6 @@ const CredentialsPage = () => {
         )}
       </ul>
 
-
       <FooterBtn
         mode="light"
         label="Education"
@@ -224,7 +73,6 @@ const CredentialsPage = () => {
           ))
         )}
       </ul>
-
 
       <FooterBtn
         mode="light"
