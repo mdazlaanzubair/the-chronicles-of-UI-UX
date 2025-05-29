1aacfc5e1db4276a401acc3c3890438ed5d708d6
Muhammad Azlaan Zubair <mdazlaan1996@gmail.com>
2025-05-29 21:23:31 +0500

    code cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,9 @@ import { DarkModeProvider } from "./provider/DarkModeProvider.tsx";
 import { RouterProvider } from "react-router-dom";
 import { app_router } from "./routes/routes.tsx";
 
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DarkModeProvider>
-      {/* <App /> */}
       <RouterProvider router={app_router} />
     </DarkModeProvider>
   </StrictMode>
