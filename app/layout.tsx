22640b5179fc3311de4d35bf1be0c9a184d841db
Muhammad Azlaan Zubair <mdazlaanzubair@Muhammads-MacBook-Pro.local>
2026-07-23 16:54:40 +0500

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
-import { Geist, Geist_Mono, Inter } from "next/font/google"
+import { Geist, Geist_Mono, Oxanium, Playfair_Display } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
-const inter = Inter({subsets:['latin'],variable:'--font-sans'})
+const playfairDisplayHeading = Playfair_Display({subsets:['latin'],variable:'--font-heading'});
+
+const oxanium = Oxanium({subsets:['latin'],variable:'--font-sans'})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,7 +22,7 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", inter.variable)}
+      className={cn("antialiased", fontMono.variable, "font-sans", oxanium.variable, playfairDisplayHeading.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
