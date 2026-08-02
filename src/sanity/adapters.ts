5d5a92eea863241aca6d317a5fae27dec17879dc
Muhammad Azlaan Zubair <mdazlaanzubair@Muhammads-MacBook-Pro.local>
2026-08-03 00:19:48 +0500

    refactor: remove unused case study, writing, skills, and interests Sanity queries and adapters

--- src/sanity/adapters.ts
+++ src/sanity/adapters.ts
@@ -2,25 +2,17 @@ import type { PortableTextBlock } from "next-sanity"
 
 import type {
   ACADEMIC_HISTORY_QUERY_RESULT,
-  CASE_STUDY_DETAIL_QUERY_RESULT,
   EXPERIENCE_QUERY_RESULT,
-  INTERESTS_QUERY_RESULT,
   PUBLICATIONS_LIST_QUERY_RESULT,
-  SKILLS_QUERY_RESULT,
   SOCIAL_PROFILES_QUERY_RESULT,
   WORK_LIST_QUERY_RESULT,
-  WRITING_DETAIL_QUERY_RESULT,
 } from "@/sanity.types"
 import type {
   AcademicInterface,
-  CaseStudyInterface,
   ExperienceInterface,
-  InterestType,
   PublicationInterface,
-  SkillInterface,
   SocialMediaInterface,
   WorkInterface,
-  WritingInterface,
 } from "@/type"
 
 /**
@@ -38,20 +30,6 @@ const asPortableTextBlocks = (body: unknown): PortableTextBlock[] =>
 export const toPublications = (
   documents: PUBLICATIONS_LIST_QUERY_RESULT
 ): PublicationInterface[] => documents
-
-export const toCaseStudy = (
-  document: NonNullable<CASE_STUDY_DETAIL_QUERY_RESULT>
-): CaseStudyInterface => ({
-  ...document,
-  body: asPortableTextBlocks(document.body),
-})
-
-export const toWriting = (
-  document: NonNullable<WRITING_DETAIL_QUERY_RESULT>
-): WritingInterface => ({
-  ...document,
-  body: asPortableTextBlocks(document.body),
-})
 
 export const toWorkItems = (
   documents: WORK_LIST_QUERY_RESULT
@@ -78,12 +56,6 @@ export const toAcademicHistory = (
   documents: ACADEMIC_HISTORY_QUERY_RESULT
 ): AcademicInterface[] => documents
 
-export const toSkills = (skills: SKILLS_QUERY_RESULT): SkillInterface[] =>
-  skills
-
 export const toSocialProfiles = (
   profiles: SOCIAL_PROFILES_QUERY_RESULT
 ): SocialMediaInterface[] => profiles
-
-export const toInterests = (interests: INTERESTS_QUERY_RESULT): InterestType =>
-  interests
